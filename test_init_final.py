f2749f48cfebde790dbd0f0d3652b6a06ce06b81
airdropbird <41575113+airdropbird@users.noreply.github.com>
2021-12-26 00:52:44 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -2259,7 +2259,33 @@ class mainCog(commands.Cog):
 				return
 		else:  
 			return
+	
+	@commands.command(name=command[46][0], aliases=command[46][1:])
+	async def playText_(self, ctx):
+		if ctx.message.channel.id == basicSetting[7]:
+			if basicSetting[21] != "1":
+				return await ctx.send('```보이스를 사용하지 않도록 설정되어 있습니다.```', tts=False)
 
+			msg = ctx.message.content[len(ctx.invoked_with)+1:]
+			sayMessage = msg
+			try:
+				await MakeSound(sayMessage, './sound/say')
+			except:
+				await ctx.send( f"```음성파일 생성에 실패하였습니다.!(amazon polly 사용시 키 값을 확인하세요!)```")
+				return
+			await ctx.send("```< " + ctx.author.display_name + " >님이 \"" + sayMessage + "\"```", tts=False)
+			try:
+				if aws_key != "" and aws_secret_key != "":
+					await PlaySound(ctx.voice_client, './sound/say.mp3')
+				else:
+					await PlaySound(ctx.voice_client, './sound/say.wav')
+			except:
+				await ctx.send( f"```음성파일 재생에 실패하였습니다. 접속에 문제가 있거나 음성채널에 접속 되지 않은 상태입니다.!```")
+				return
+		else:  
+			return
+
+		
 	################ 리젠시간 출력 ################
 	@commands.command(name=command[17][0], aliases=command[17][1:])
 	async def regenTime_(self, ctx):
